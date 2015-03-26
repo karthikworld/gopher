104e43caba8ba2176ae9b918fa27f1294e1246d4
Karthikeyan <root@Karthikeyan-MacBook-Pro.local>
2015-03-26 09:11:54 +0530

    Algorithm - Insertion sort

---
+++ Algorithms/Insertion Sort.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main () {
+	
+	//var x [5]float64
+
+	a :={5,2,4,6,1,3}float64
+
+	key := 0
+
+	for j :=2 j < 6; j++ {
+		key = a[j]
+		i = j-1
+		for i>0 &&
+
+	} 
+}
